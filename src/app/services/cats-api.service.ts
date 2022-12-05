14f663c51ef5892337948ddcbb41f17d27d88f48
asm-dev <salamanca.m@outlook.com>
2022-12-05 17:19:02 +0100

    add data and service to connect with API

--- src/app/services/cats-api.service.ts
+++ src/app/services/cats-api.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { delay, Observable } from 'rxjs';
+import { Cat } from '../models/cat.model';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CatsAPIService {
 
-  constructor() { }
+  readonly BASE_URL = 'http://localhost:8000';
+ 
+  constructor(private http: HttpClient) { }
+ 
+  getCats(): Observable<Cat[]> {
+    return this.http.get<Cat[]>(`${this.BASE_URL}/cats`).pipe(
+      delay(3000)
+    );
+  }
 }
